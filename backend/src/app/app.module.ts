ca082a5a92fd46d8936c5de76ad07254630987c4
tony.ng <ngkokhong.tony@gmail.com>
2025-05-01 17:19:01 +0800

    added mongoose lib and user entity

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController, PushNotificationController, NotifySecondUserController } from './app.controller';
 import { AppService, TasksService, PushNotificationService, NotifySecondUserService, AcknowledgementFromFirstUserService } from './app.service';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@checkin.wl6j8q4.mongodb.net/?retryWrites=true&w=majority&appName=CheckIn`,
+    ),],
   exports: [],
   controllers: [AppController, PushNotificationController, NotifySecondUserController],
   providers: [AppService, PushNotificationService, TasksService, NotifySecondUserService, AcknowledgementFromFirstUserService],
---
+++ backend/src/user/users.entity.ts
@@ -0,0 +1,14 @@
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    mobileNumber: { type: String, required: true },
+});
+
+export interface User extends mongoose.Document {
+    id: string;
+    firstName: string;
+    lastName: string;
+    mobileNumber: string;
+}
