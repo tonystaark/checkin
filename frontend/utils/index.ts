513cb7b33dd869e9d2f5de41992027ebdd45b3b6
tony.ng <ngkokhong.tony@gmail.com>
2025-07-29 08:12:34 +0800

    make EXPO_PUBLIC_API_URL to be localhost while in development mode

--- frontend/utils/index.ts
+++ frontend/utils/index.ts
@@ -1,3 +1,2 @@
 export const removeWhitespaces = (arg: string) => arg.replace(/\s+/g, '')
-// export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.0.104:3000';
-export const API_URL = 'http://192.168.0.229:3000';
+export const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.0.229:3000';
